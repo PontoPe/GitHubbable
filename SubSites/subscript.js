2639b74e0ed43bf0ad66e321837dfc0bbcf5500c
PontoPe <pegradowski@hotmail.com>
2024-09-26 16:55:40 -0300

    trying to unbreak

--- SubSites/subscript.js
+++ SubSites/subscript.js
@@ -1,19 +1,19 @@
-var track = document.getElementById("image-track");
+const track = document.getElementById("image-track");
 
-var handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
 
-var handleOnUp = () => {
+const handleOnUp = () => {
   track.dataset.mouseDownAt = "0";  
   track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-var handleOnMove = e => {
+const handleOnMove = e => {
   if(track.dataset.mouseDownAt === "0") return;
   
-  var mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
   
-  var percentage = (mouseDelta / maxDelta) * -100,
+  const percentage = (mouseDelta / maxDelta) * -100,
         nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
         nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
   
@@ -23,7 +23,7 @@ var handleOnMove = e => {
     transform: `translate(${nextPercentage}%, -50%)`
   }, { duration: 1200, fill: "forwards" });
   
-  for(var image of track.getElementsByClassName("image")) {
+  for(const image of track.getElementsByClassName("image")) {
     image.animate({
       objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
